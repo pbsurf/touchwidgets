2707092c01f3954ea51a32db888ad039a5183bff
pbsurf <pbsurf@gmail.com>
2014-02-25 11:03:07 -0800

    initial import of TouchApplication

---
+++ touchapplication.h
@@ -0,0 +1,25 @@
+#ifndef TOUCHAPPLICATION_H
+#define TOUCHAPPLICATION_H
+
+#include <QApplication>
+
+class TouchApplication : public QApplication
+{
+public:
+  TouchApplication(int& argc, char** argv);
+
+  bool notify(QObject* receiver, QEvent* event);
+
+  static int tabletButtons() { return m_tabletButtons; }
+  static void setTabletButtons(int btns) { m_tabletButtons = btns; }
+
+private:
+  bool sendMouseEvent(QObject* receiver, QEvent::Type mevtype, QPoint globalpos, Qt::KeyboardModifiers modifiers);
+
+  int activeTouchId;
+  int acceptCount;
+  enum {None, PassThru, TouchInput, TabletInput} inputState;
+  static int m_tabletButtons;
+};
+
+#endif
